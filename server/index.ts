1d1488ec6c1bfdc5108a526fe25470856c922c6b
Ushinji <siguma971@gmail.com>
2019-08-22 10:32:10 +0900

    Add apollo-server-express

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,28 @@
 import http from 'http';
 import express from 'express';
+import { gql, ApolloServer } from 'apollo-server-express';
 import { logger, requestLogger } from './middlewares/logger';
 import routes from './routes';
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(requestLogger);
+apolloServer.applyMiddleware({ app });
 
 app.use('/', routes);
 
